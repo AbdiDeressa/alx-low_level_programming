767f13231df24c6de518bf424a3cad4df6f3bd4e
AbdiDeressa <borifwegayehu@gmail.com>
2022-04-11 13:53:48 -0700

    My commit message

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,13 @@
  * Return: Always 0 (success/correct)
  */
 int main(void)
-
 {
 	char alphabet;
-
-	for ( alphabet = 'a'; alphabet <= 'z'; alphabet++ )  
+	for ( alphabet = 'a'; alphabet <= 'z'; alphabet++)  
 	{
 		putchar(alphabet);
 	}
 
 	putchar('\n');
-
 	return(0);
 }
