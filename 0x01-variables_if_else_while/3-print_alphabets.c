acf9da68dee09db772b4da95cd251ae73b64bc14
Abdi Wegayehu Deressa <101544628+AbdiDeressa@users.noreply.github.com>
2022-04-12 01:18:19 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,23 +4,22 @@
  *
  * Return: Always 0 (success/correct)
  */
-int main (void)
+int main(void)
 
 {
 	char lower_case;
 	char upper_case;
 
-	for(upper_case = 'A'; upper_case <= 'z'; upper_case++)
+	for (upper_case = 'A'; upper_case <= 'z'; upper_case++)
 	{
 		putchar(upper_case);
 	}
 
 
-	for(lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
 		putchar(lower_case);
 	}
-
 	
 	return (0);
 }
